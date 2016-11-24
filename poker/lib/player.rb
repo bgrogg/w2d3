d90cb373d080279b10207e66e80fa1c5da82dd30
App Academy Student <student@appacademy.io>
2016-11-23 18:00:27 -0800

    Working on Player class

--- poker/lib/player.rb
+++ poker/lib/player.rb
@@ -1,5 +1,11 @@
-
+require "hand"
 
 class Player
-  
+
+  attr_reader :hand, :pot
+
+  def initialize(hand, pot)
+    @hand, @pot = hand, pot
+  end
+
 end
--- poker/spec/player_spec.rb
+++ poker/spec/player_spec.rb
@@ -1,6 +1,35 @@
 require "rspec"
 require "player"
 
-class Player
+describe "Player" do
+  let (:card2) { Card.new("10", "club") }
+  let (:card3) { Card.new("8", "club") }
+  let (:card4) { Card.new("10", "heart") }
+  let (:card5) { Card.new("K", "diamond") }
+  let (:card6) { Card.new("10", "spade") }
+  let(:hand) { [card2, card3, card4, card5, card6] }
+  let(:player1) { Player.new(hand, 100) }
+
+  describe "#initialize" do
+    it "sets up a player with a hand and a pot" do
+      expect(player1.hand.length).to eq(5)
+      expect(player1.pot).to be_truthy
+    end
+
+  end
+
+  describe "#discard" do
+    it "asks the player which cards they want to discard cards" do
+
+    end
+
+  end
+
+  describe "#bet" do
+    it "asks the player if they wish to fold, see, or raise" do
+  
+    end
+  end
+
 
 end
